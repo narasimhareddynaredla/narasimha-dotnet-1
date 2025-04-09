776f64f9305f82c4feda157c748a2b98f5993298
narasimhareddynaredla <narasimhareddynaredla@gmail.com>
2025-04-09 10:06:29 +0530

    adding the src

---
+++ Controllers/HtttpController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace ToDOList.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HtttpController : ControllerBase
+    {
+        private readonly string _connectionSting;
+        private readonly IConfiguration _configuration;
+        private readonly IHttpClientFactory _clientFactory;
+
+        public HtttpController(IConfiguration Configuration, IHttpClientFactory clientFactory)
+        {
+            _configuration = Configuration;
+            _clientFactory = clientFactory;
+            _connectionSting = _configuration.GetValue<string>("httpAPIEndpoint");
+        }
+
+        [HttpGet(Name = "GetHttpResponse")]
+        public async Task<string> Get()
+        {
+            var request = new HttpRequestMessage(HttpMethod.Get, _connectionSting + "Version");
+
+            var client = _clientFactory.CreateClient();
+
+            var response = await client.SendAsync(request);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var responseStream = await response.Content.ReadAsStringAsync();
+                //string todolist = await JsonSerializer.Deserialize<string>(responseStream);
+                return "Success:" + responseStream;
+            }
+            else
+            {
+                return "Failure";
+            }
+        }
+    }
+}
